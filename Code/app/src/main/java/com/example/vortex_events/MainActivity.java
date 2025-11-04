a718cac83dcab7e72444985ce4653de944ef5f03
Salty876 <salabdoulaye876@gmail.com>
2025-11-03 18:20:40 -0700

    deleted viewmodel
    # Conflicts:
    #	Code/app/src/main/java/com/example/vortex_events/MainActivity.java
    
    # Conflicts:
    #	Code/app/src/main/java/com/example/vortex_events/MainActivity.java

--- Code/app/src/main/java/com/example/vortex_events/MainActivity.java
+++ Code/app/src/main/java/com/example/vortex_events/MainActivity.java
@@ -45,11 +45,14 @@ public class MainActivity extends AppCompatActivity {
         );
 
 
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         //Add to every activity
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
