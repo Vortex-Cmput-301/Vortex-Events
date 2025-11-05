1514e7c6b7de248f091fabf8668609039fb19c08
Benjamin Nguyen <BenNguyenv182@gmail.com>
2025-11-04 20:21:44 -0700

    added functionality of events page, details button doesnt work yet but adding events should work if implemented correctly

--- Code/app/src/main/java/com/example/vortex_events/ExplorePage.java
+++ Code/app/src/main/java/com/example/vortex_events/ExplorePage.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -43,52 +46,52 @@ public class ExplorePage extends AppCompatActivity {
             startActivity(intent);
         });
 
-            bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-                //Add the rest of the activities when finished
-                //made a boolean function to implement highlighting items. will implement later
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    int itemId = item.getItemId();
-                    if (itemId == R.id.nav_home) {
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                        return true;
-                    } else if (itemId == R.id.nav_create) {
-                        Intent intent = new Intent(getApplicationContext(), CreateActivityEvents.class);
-                        startActivity(intent);
-                        return true;
-                    }
-                    return false;
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            //Add the rest of the activities when finished
+            //made a boolean function to implement highlighting items. will implement later
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_home) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_create) {
+                    Intent intent = new Intent(getApplicationContext(), CreateActivityEvents.class);
+                    startActivity(intent);
+                    return true;
                 }
-            });
-        }
-
-            //TODO THis is just a test to see if events list work, need to get events from DATABASE in order to finish.
-            private void setupEventListData() {
-                eventList = new ArrayList<>();
-                ArrayList<String> arraylist = new ArrayList<String>();
-                arraylist.add("trending");
-                arraylist.add("local");
-
-
-                Calendar calendar = Calendar.getInstance();
-
-                // Create the enrollment start date: October 1, 2025
-                calendar.set(2025, Calendar.OCTOBER, 1); // Month is 0-indexed, OCTOBER is 9
-                Date enrollmentStart = calendar.getTime();
-
-                // Create the enrollment end date: November 15, 2025
-                calendar.set(2025, Calendar.NOVEMBER, 15); // NOVEMBER is 10
-                Date enrollmentEnd = calendar.getTime();
-
-                // Create the event start/end date: November 20, 2025
-                calendar.set(2025, Calendar.NOVEMBER, 20);
-                Date eventDate = calendar.getTime();
-
-
-                Event first_event = new Event("Scream", "UofA", "Bonnie", "123456", enrollmentStart, enrollmentEnd, eventDate, eventDate, arraylist, "description", 20);
-                eventList.add(first_event);
+                    return false;
             }
-        }
+        });
+    }
+
+    //TODO THis is just a test to see if events list work, need to get events from DATABASE in order to finish.
+    private void setupEventListData() {
+        eventList = new ArrayList<>();
+        ArrayList<String> arraylist = new ArrayList<String>();
+        arraylist.add("trending");
+        arraylist.add("local");
+
+
+        Calendar calendar = Calendar.getInstance();
+
+        // Create the enrollment start date: October 1, 2025
+        calendar.set(2025, Calendar.OCTOBER, 1); // Month is 0-indexed, OCTOBER is 9
+        Date enrollmentStart = calendar.getTime();
+
+        // Create the enrollment end date: November 15, 2025
+        calendar.set(2025, Calendar.NOVEMBER, 15); // NOVEMBER is 10
+        Date enrollmentEnd = calendar.getTime();
+
+        // Create the event start/end date: November 20, 2025
+        calendar.set(2025, Calendar.NOVEMBER, 20);
+        Date eventDate = calendar.getTime();
+
+
+        Event first_event = new Event("Scream", "UofA", "Bonnie", "123456", enrollmentStart, enrollmentEnd, eventDate, eventDate, arraylist, "description", 20);
+        eventList.add(first_event);
+    }
+}
 
 
