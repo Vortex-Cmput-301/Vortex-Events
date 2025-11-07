ed2022772c19b43d8e2ccf0e1bcfb0a7dc9bda4f
asehmb <a.sehmbi@outlook.com>
2025-11-07 13:41:16 -0700

    added a search

---
+++ Code/app/src/main/java/com/example/vortex_events/SearchEvents.java
@@ -0,0 +1,119 @@
+package com.example.vortex_events;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search_events);
+
+        DatabaseWorker db = new DatabaseWorker();
+        EditText searchBar = findViewById(R.id.searchBar);
+        final String[] stringToFilterOut = {"seeyuh"};
+        List<String> filteredEvents = new ArrayList<>();
+        ArrayAdapter<String> searchResults = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                filteredEvents);
+        ListView listView = findViewById(R.id.searchResults);
+        listView.setAdapter(searchResults);
+
+        searchBar.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                stringToFilterOut[0] = s.toString();
+                db.getAllEvents().addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        filteredEvents.clear();
+
+                        List<Event> allEvents = task.getResult().toObjects(Event.class);
+
+                        for (Event event : allEvents) {
+
+                            // Add a null check before calling .contains()
+                            if (event.getTags() != null) {
+                                for (String tag : event.getTags()) {
+                                    if (tag.toLowerCase().contains(stringToFilterOut[0].toLowerCase())) {
+                                        filteredEvents.add(event.getName());
+                                        break; // Stop checking other tags once we found a match
+                                    }
+                                }
+                            }
+                        }
+                        searchResults.notifyDataSetChanged();
+
+                        Log.d("Firestore", "Filtered events count: " + filteredEvents.size());
+
+                    } else {
+                        Log.w("Firestore", "Error getting documents.", task.getException());
+                    }
+                });
+
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Add to every activity
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            //Add the rest of the activities when finished
+            //made a boolean function to implement highlighting items. will implement later
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_home){
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    return true;
+                }else if(itemId == R.id.nav_create) {
+                    Intent intent = new Intent(getApplicationContext(), CreateActivityEvents.class);
+                    startActivity(intent);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
