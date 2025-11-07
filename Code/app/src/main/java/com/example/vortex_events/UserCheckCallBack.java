2c0d313a0e570fcf0c65d8916c45d505ff5459fc
Salty876 <salabdoulaye876@gmail.com>
2025-11-06 22:30:28 -0700

    ready for main

--- Code/app/src/main/java/com/example/vortex_events/UserCheckCallBack.java
+++ Code/app/src/main/java/com/example/vortex_events/UserCheckCallBack.java
@@ -2,5 +2,7 @@ package com.example.vortex_events;
 
 public interface UserCheckCallBack {
     void onUserChecked(boolean exists);
+    void rUserCollected(RegisteredUser user);
+    Void gUserCollected(GuestUser user);
     
 }
