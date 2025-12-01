443a1f5e858d524ea1cf3317090c1b221ec1ae27
jiutianxvanyin <kehan4@ualberta.ca>
2025-11-30 20:06:22 -0700

    a merge of my branch

--- Code/app/src/main/java/com/example/vortex_events/EventDetails.java
+++ Code/app/src/main/java/com/example/vortex_events/EventDetails.java
@@ -148,6 +148,7 @@ public class EventDetails extends AppCompatActivity {
             if (documentSnapshot.exists()) {
                 Log.d("OrganizerViewParticipant", "Event 'accepted' field: " + documentSnapshot.get("accepted"));
                 Event event = dbWorker.convertDocumentToEvent(documentSnapshot);//use method in DatabaseWorker instead
+                assert event != null;
                 description = event.getDescription();
                 title = event.getName();
                 capacity =  event.getCapacity();
@@ -169,10 +170,7 @@ public class EventDetails extends AppCompatActivity {
                     } catch (Exception e) {
                         Log.e("EventDetails", "Failed to load image", e);
                     }
-                } else {
-                    eventPoster.setVisibility(View.GONE);
                 }
-
 
 
 
@@ -223,9 +221,9 @@ public class EventDetails extends AppCompatActivity {
                     signupButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-//                            Intent intent = new Intent(EventDetails.this, EditEvents.class);
-//                            intent.putExtra("EventID", EventID);
-//                            startActivity(intent);
+                           Intent intent = new Intent(EventDetails.this, EditEvents.class);
+                          intent.putExtra("EventID", EventID);
+                          startActivity(intent);
                         }
                     });
 
